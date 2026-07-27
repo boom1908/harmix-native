20cbaaaccfa6ecb41d9b92b91c80bc57f22a55af
boom1908 <boomgonza1908@gmail.com>
2026-07-27 12:02:38 +0000

    fix: add missing clickable import to player screen

--- app/src/main/java/com/boom/harmix/ui/screens/FullScreenPlayerScreen.kt
+++ app/src/main/java/com/boom/harmix/ui/screens/FullScreenPlayerScreen.kt
@@ -1,4 +1,5 @@
 package com.boom.harmix.ui.screens
+import androidx.compose.foundation.clickable
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
